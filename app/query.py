8b793c5054808bad3b6a72b67336fb5e5359c8b2
SARA SIMON <smbsimon@gmail.com>
2015-09-15 16:02:46 -0400

    Most recent podcast episode plays for each podcast

--- app/query.py
+++ app/query.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 from sheet import get_google_sheet
 from slugify import slugify
+import requests
+from BeautifulSoup import BeautifulSoup
 
 def get_podcasts():
     sheet = get_google_sheet()
@@ -10,5 +12,14 @@ def get_podcasts():
         if podcast['Category']:
             podcast['slug'] = slugify(podcast['Name'])
             podcast_list.append(podcast)
+
+        if podcast['RSS']:
+            feed_url = podcast['RSS']
+            response = requests.get(feed_url)
+            html = response.content
+            soup = BeautifulSoup(html)
+
+            podcast['recent_one'] = soup.enclosure['url']
+            podcast['title_one'] = soup.item.title.string
 
     return podcast_list
