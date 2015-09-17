e2e45ca42116534f642f0bce22c2fcf9328e6447
SARA SIMON <smbsimon@gmail.com>
2015-09-17 16:42:11 -0400

    Podcast view page now shows three most recent episodes. Empty quotes in lines 22-23 of the _podcast_view.html template is not my favorite, but if I want to use the same macros template ~and~ keep the 'Featured Epidode' text on the homepage, I've got to give it a third argument for copy. Also, there's got to be some refactoring to be done in the query method. Another day... Another time...

--- app/query.py
+++ app/query.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 from sheet import get_google_sheet
 from slugify import slugify
-import requests
 from BeautifulSoup import BeautifulSoup
+import requests
+import re
 
 def get_podcasts():
     sheet = get_google_sheet()
@@ -19,7 +20,18 @@ def get_podcasts():
             html = response.content
             soup = BeautifulSoup(html)
 
-            podcast['recent_one'] = soup.enclosure['url']
-            podcast['title_one'] = soup.item.title.string
+            all_enclosure_urls = soup.findAll(re.compile('^enclosure'))
+            recent_episodes = [tag['url'] for tag in all_enclosure_urls]
+
+            recent_titles = soup.findAll('title')
+
+            podcast['recent_zero'] = recent_episodes[0]
+            podcast['title_zero'] = recent_titles[2].string
+
+            podcast['recent_one'] = recent_episodes[1]
+            podcast['title_one'] = recent_titles[3].string
+
+            podcast['recent_two'] = recent_episodes[2]
+            podcast['title_two'] = recent_titles[4].string
 
     return podcast_list
