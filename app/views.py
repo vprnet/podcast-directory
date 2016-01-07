c38963bcbdf8cfba762adc55c4ec7e272a50e0d1
SARA SIMON <smbsimon@gmail.com>
2016-01-07 08:57:40 -0500

    Remove hardcoded spotlight feature information: Step no. 1 in cleaning up the Google spreadsheet columns serving as the database. There's a better way to do this. Incoming.

--- app/views.py
+++ app/views.py
@@ -21,8 +21,6 @@ def index():
     page_url = BASE_URL + request.path
     page_title = "VPR's Podcast Directory"
     landing = True
-    # spotlight_url will need to change every time we swap out a featured podcast
-    spotlight_url = "http://www.vpr.net/apps/podcasts/wintry-mix"
 
     social = project_social
 
@@ -32,8 +30,7 @@ def index():
         podcasts=podcasts,
         landing=landing,
         project_social=project_social,
-        page_url=page_url,
-        spotlight_url=spotlight_url)
+        page_url=page_url)
 
 
 @app.route('/<Name>')
